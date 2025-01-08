4f97ccd574c0cc708a4404c68efeb98c6c46a1aa
ruthcaswellsmith <ruth@plannuh.com>
2025-01-08 14:29:37 -0500

    Day 7 More Intcode computer

--- Day7.py
+++ Day7.py
@@ -1,33 +1,55 @@
 from __future__ import annotations
 
 from typing import Tuple
-from functools import reduce
 from itertools import permutations
 
 from utils import read_file
-from Day5 import Program
+from Day5 import Program, Status
 
 
 class Amplifier:
     def __init__(self, line: str, phase: int):
         self.program = Program(line)
-        self.phase = phase
+        self.add_input(phase)
+        self.next = None
 
-    def run(self, input_signal: int):
-        self.program.run([self.phase, input_signal])
-        return self
+    @property
+    def outputs(self):
+        return self.program.outputs
+
+    @property
+    def status(self):
+        return self.program.status
+
+    def run(self):
+        self.program.run()
+
+    def add_input(self, val: int):
+        self.program.input_handler.add_input(val)
+
+    def get_output(self):
+        return self.program.outputs.popleft()
 
 
 class AmplifierPipeline:
-    def __init__(self, line: str, phases: Tuple[int]):
+    def __init__(self, line: str, phases: Tuple):
         self.amplifiers = [Amplifier(line, phase) for phase in phases]
+        for i in range(len(self.amplifiers)-1):
+            self.amplifiers[i].next = self.amplifiers[i+1]
+        self.amplifiers[len(self.amplifiers)-1].next = self.amplifiers[0]
+        self.amplifiers[0].add_input(0)
+
+    @property
+    def thruster_output(self):
+        return self.amplifiers[0].program.input_handler.values.pop()
 
     def run(self):
-        return reduce(
-            lambda input_val, amplifier: amplifier.run(input_val).program.outputs[-1],
-            self.amplifiers,
-            0
-        )
+        while not all([amp.status == Status.TERMINATED for amp in self.amplifiers]):
+            for amp in self.amplifiers:
+                amp.run()
+                while amp.outputs:
+                    amp.next.add_input(amp.get_output())
+        return self.thruster_output
 
 
 def main():
@@ -35,8 +57,12 @@ def main():
     data = read_file(filename)
 
     phases = [ele for ele in range(5)]
-    results = [AmplifierPipeline(data[0], permutation).run() for permutation in permutations(phases, 5)]
+    results = [AmplifierPipeline(data[0], p).run() for p in permutations(phases, 5)]
     print(f"The answer to part 1 is {max(results)}")
+
+    phases = [ele for ele in range(5, 10)]
+    results = [AmplifierPipeline(data[0], p).run() for p in permutations(phases, 5)]
+    print(f"The answer to Part 2 is {max(results)}")
 
 
 if __name__ == '__main__':
